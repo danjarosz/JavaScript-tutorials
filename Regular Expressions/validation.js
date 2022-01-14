119196d22628d84e5f192d657af896427c33ea8d
Daniel Jarosz <jarosz.daniel@protonmail.com>
2022-01-14 22:28:17 +0100

    feat(validate.js): general structure

--- Regular Expressions/validation.js
+++ Regular Expressions/validation.js
@@ -1,3 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("works")
+    const patterns = {
+        telephone: /^\d{11}$/,
+    }
+
+    const validate = (field, regex) => {
+        const isValid = field.value.match(regex);
+        if (isValid) {
+            field.classList.remove("invalid")
+            field.classList.add("valid")
+        } else {
+            field.classList.remove("valid")
+            field.classList.add("invalid")
+        }
+    }
+
+    const Form = document.querySelector("form");
+    const inputs = Form.querySelectorAll("input");
+
+    inputs.forEach(input => {
+        input.addEventListener("keyup", (e) => {
+           const field = e.target;
+           validate(field, patterns[field.name]);
+        })
+    })
 })
